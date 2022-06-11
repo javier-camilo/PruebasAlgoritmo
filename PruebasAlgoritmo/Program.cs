14eea0627d3a1a80f51dd660f2c4bd0a55500e7b
javier-camilo <camiloviloriatovar@gmail.com>
2022-06-11 16:24:11 -0500

    Agregar archivos de proyecto.

---
+++ PruebasAlgoritmo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebasAlgoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
